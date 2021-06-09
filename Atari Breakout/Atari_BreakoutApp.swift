dcefbbda91cd6b1a5af0fe4d6a210126f0769feb
Jake Glick <jake@glickpeople.com>
2021-06-09 14:34:22 +1000

    Initial Commit

---
+++ Atari Breakout/Atari_BreakoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atari_BreakoutApp.swift
+//  Atari Breakout
+//
+//  Created by Jake Glick on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Atari_BreakoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
